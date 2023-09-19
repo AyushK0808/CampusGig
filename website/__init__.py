34c042ed4c09a39fa966dff7630bc95cfe1f66df
unknown <vishnuvpk05@gmail.com>
2023-09-20 00:11:38 +0530

    fix: added templates and static

--- website/__init__.py
+++ website/__init__.py
@@ -1,7 +1,20 @@
 from flask import Flask 
-
-
+from os import path
+from flask_mysqldb import MySQL
 def create_app():
+    from .auth import auth
+    from .views import views
     app = Flask(__name__)
+
+    app.register_blueprint(views,url_prefix='/')
+    app.register_blueprint(auth,url_prefix='/')
+
+    app.config['MYSQL_HOST'] = 'localhost'
+    app.config['MYSQL_USER'] = 'root'
+    app.config['MYSQL_PASSWORD'] = 'root'
+    app.config['MYSQL_DB'] = 'hackbattle'
+    app.secret_key = 'your secret key'
+    mysql = MySQL(app)
+
 
     return app 
--- website/auth.py
+++ website/auth.py
@@ -1,30 +1,20 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Blueprint,Flask, render_template, request, redirect, url_for, session
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
 import re
+
+
+auth = Blueprint('auth', __name__)
  
- 
-app = Flask(__name__)
- 
- 
-app.secret_key = 'your secret key'
- 
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'your password'
-app.config['MYSQL_DB'] = 'geeklogin'
- 
-mysql = MySQL(app)
- 
-@app.route('/')
-@app.route('/login', methods =['GET', 'POST'])
+@auth.route('/')
+@auth.route('/login', methods =['GET', 'POST'])
 def login():
     msg = ''
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
         username = request.form['username']
         password = request.form['password']
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM accounts WHERE username = % s AND password = % s', (username, password, ))
+        cursor.execute('SELECT * FROM user WHERE username = % s AND password = % s', (username, password, ))
         account = cursor.fetchone()
         if account:
             session['loggedin'] = True
@@ -36,14 +26,14 @@ def login():
             msg = 'Incorrect username / password !'
     return render_template('login.html', msg = msg)
  
-@app.route('/logout')
+@auth.route('/logout')
 def logout():
     session.pop('loggedin', None)
     session.pop('id', None)
     session.pop('username', None)
     return redirect(url_for('login'))
  
-@app.route('/register', methods =['GET', 'POST'])
+@auth.route('/register', methods =['GET', 'POST'])
 def register():
     msg = ''
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form :
@@ -51,7 +41,7 @@ def register():
         password = request.form['password']
         email = request.form['email']
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM accounts WHERE username = % s', (username, ))
+        cursor.execute('SELECT * FROM user WHERE username = % s', (username, ))
         account = cursor.fetchone()
         if account:
             msg = 'Account already exists !'
