03236d1553f1fb9d34ce6c18fe1de3c04aab8c59
Sashank Ravipati <sashankr04@gmail.com>
2023-09-20 13:14:54 +0530

    feat: recruit route

--- website/views.py
+++ website/views.py
@@ -1,11 +1,12 @@
 from flask import Flask,Blueprint,render_template,session,request,redirect,url_for,flash
 from flask_login import login_required, current_user
-
+import time
 import mysql.connector
+
 db_connection = mysql.connector.connect(
 host='localhost',
 user='root',
-password='root',
+password='mysql',
 database='hackbattle'
 )
 cursor=db_connection.cursor()
@@ -33,3 +34,42 @@ def edit_profile():
         selected_skills = request.form.getlist('skills[]')
     print(selected_skills)
     return render_template("editprofile.html",user=current_user,skills=skills_data)
+
+@login_required
+@views.route('/recruit', methods=['GET', 'POST'])
+def recruit():
+    user = current_user.role
+    if user == "student":
+        return redirect("/")
+    user_id = current_user.id
+    sql = "SELECT * FROM project WHERE user_id = %s"
+    cursor.execute(sql, (user_id,))
+    jobs = cursor.fetchall()
+    
+    return render_template("recruit.html", user=current_user,jobs=jobs)
+
+
+@login_required
+@views.route('/recruitform',methods=['GET','POST'])
+def recruitform():
+    user = current_user.role
+    if user == "student":
+        return redirect("/")
+
+    if request.method == 'POST':
+        job_title = request.form.get('job_title')
+        description = request.form.get('description')
+        budget = float(request.form.get('budget'))  # Convert budget to decimal
+        skills = ', '.join(request.form.getlist('skills[]'))  # Convert skills list to a comma-separated string
+        status = 1  # Convert to 1 for True or 0 for False
+        date = time.strftime('%Y-%m-%d')  # Format the date as 'YYYY-MM-DD'
+        user_id = current_user.id
+        flash('Job listing created successfully', 'success')
+
+        sql = "INSERT INTO project (title, description, budget, skills_required, user_id, date, status) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+        values = (job_title, description, budget, skills, user_id, date, status)
+
+        cursor.execute(sql, values)
+        db_connection.commit()
+        return redirect(url_for('views.recruit'))
+    return render_template("recruitform.html")
